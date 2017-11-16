d8f8d1f765051838ab64a2ebf737f6642a84109c
Prashant Pol <developer.prashant.pol@gmail.com>
2017-11-16 20:54:53 +0900

    Modify to add Variables

---
+++ app/src/main/java/com/polstech/kotlinsamples/_1_variables/Car.java
@@ -0,0 +1,36 @@
+package com.polstech.kotlinsamples._1_variables;
+
+import _1_variables.CarKt;
+
+/**
+ * Created by prashantspol on 11/9/2017.
+ */
+
+public class Car {
+    String color;
+    Boolean dieselEngine;
+
+    public Car(String color, Boolean dieselEngine) {
+        this.color = color;
+        this.dieselEngine = dieselEngine;
+
+        CarKt carKt = new CarKt("model", true);
+        carKt.setColor("red");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Boolean getDieselEngine() {
+        return dieselEngine;
+    }
+
+    public void setDieselEngine(Boolean dieselEngine) {
+        this.dieselEngine = dieselEngine;
+    }
+}
---
+++ app/src/main/kotlin/_1_variables/CarKt.kt
@@ -0,0 +1,19 @@
+package _1_variables
+
+import com.polstech.kotlinsamples._1_variables.Car
+
+/**
+ * Created by prashantspol on 11/9/2017.
+ */
+
+
+class CarKt(private var model : String, val dieselEngine:Boolean) {
+    var color : String = "yellow"
+    private val mirrors = 2
+    lateinit var abc:String
+
+    init {
+        val car = Car("red", true)
+        car.color = "red"
+    }
+}
